f99420149b054705a899e5f9dfc9836f7bf3496f
madhu-mini1407 <krishformadhu@gmail.com>
2025-12-24 17:23:23 +0530

    initial commit

--- index.py
+++ index.py
@@ -1,25 +1,18 @@
 def myfunction(n):
-    
-    print(f"\n--- Complexity Analysis for n = {n} ---")
-    
-    print("1. First Loop Complexity: O(n) (Linear Time)")
-    for i in range(0, n + 1):
-        print("First Loop")
+    print(f"--- Complexity Analysis for n = {n} ---")
 
-    
-    print("\n2. Second Loop Complexity: O(log n) (Logarithmic Time)")
+    for i in range(0, n + 1):
+        x = i
+
     j = 1
     while j <= n + 1:
-        print("Second Loop ",j)
         j = j * 2
 
-    
-    print("\n3. Third Loop Complexity: O(1) (Constant Time)")
     for i in range(0, 100):
-        print("Third loop")
+        y = i
 
-    print("\nTotal Time Complexity: O(n)")
+    print("Execution Finished.")
 
 myfunction(10)
-print("\n" + "="*50)
+print("="*30)
 myfunction(100)
